63ab60819c038412f5bb1df301170e3f8d754a7f
Ishara Sahan Kaushalya <sahankaushalya.info@gmail.com>
2026-08-04 09:14:04 +0530

    Add Testnew class to demonstrate basic arithmetic operation

---
+++ src/day14/Testnew.java
@@ -0,0 +1,11 @@
+package day14;
+
+public class Testnew {
+    public static void main(String[] args) {
+        double a =0.1;
+        double b =0.2;
+        double sum =a+b;
+        System.out.println(sum);
+
+    }
+}
